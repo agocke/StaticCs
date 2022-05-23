2ed89993ce530caf7aa2e9f5fd4125ddce96bad7
Andy Gocke <angocke@microsoft.com>
2022-05-23 11:30:40 -0700

    Refactor a little

--- src/StaticCs.Attributes/ClosedAttribute.cs
+++
@@ -1,9 +0,0 @@
-
-using System;
-using System.Diagnostics;
-
-namespace StaticCs;
-
-[AttributeUsage(AttributeTargets.Enum)]
-[Conditional("EMIT_STATICCS_CLOSEDATTRIBUTE")]
-public sealed class ClosedAttribute : Attribute { }
---
+++ src/StaticCs.ContentFiles/ClosedAttribute.cs
@@ -0,0 +1,7 @@
+
+using System;
+
+namespace StaticCs;
+
+[AttributeUsage(AttributeTargets.Enum)]
+internal sealed class ClosedAttribute : Attribute { }
