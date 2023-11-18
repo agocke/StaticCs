b0fc87f404fae41400487bc7e89bb26fa0e6a57e
Andy Gocke <andy@commentout.net>
2023-11-18 15:36:27 -0800

    Update EqArray (#11)
    Multi-target, and add functionality

--- src/Collections/EqArray.cs
+++ src/Collections/EqArray.cs
@@ -4,39 +4,113 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using Serde;
 
 namespace StaticCs.Collections;
 
 public static class EqArray
 {
+    public static EqArray<T> ToEq<T>(this IEnumerable<T> array) => new(array.ToImmutableArray());
     public static EqArray<T> ToEq<T>(this ImmutableArray<T> array) => new(array);
     public static EqArray<T> Create<T>(params T[] array) => ImmutableArray.Create(array).ToEq();
+    public static EqArray<T> Create<T>(ReadOnlySpan<T> span) => ImmutableArray.Create(span).ToEq();
 }
 
-public readonly record struct EqArray<T>(ImmutableArray<T> Array) : IReadOnlyCollection<T>
+[SerdeWrap(typeof(EqArraySerdeWrap))]
+#if NET8_0_OR_GREATER
+[CollectionBuilder(typeof(EqArray), nameof(EqArray.Create))]
+#endif
+public readonly struct EqArray<T> : IReadOnlyCollection<T>, IEquatable<EqArray<T>>
 {
+    private readonly ImmutableArray<T> value;
+    public EqArray(ImmutableArray<T> value)
+    {
+        this.value = value;
+    }
+
+    public ImmutableArray<T> Array => value;
+
     public static readonly EqArray<T> Empty = ImmutableArray<T>.Empty.ToEq();
 
-    public int Length => Array.Length;
+    public int Length => value.Length;
 
-    int IReadOnlyCollection<T>.Count => Array.Length;
+    int IReadOnlyCollection<T>.Count => value.Length;
 
-    public bool Equals(EqArray<T> other) => Array.SequenceEqual(other.Array);
+    public override bool Equals(object? obj) => obj is EqArray<T> other && Equals(other);
 
-    public IEnumerator<T> GetEnumerator() => ((IEnumerable<T>)Array).GetEnumerator();
+    public bool Equals(EqArray<T> other) => value.SequenceEqual(other.Array);
+
+    public IEnumerator<T> GetEnumerator() => ((IEnumerable<T>)value).GetEnumerator();
 
     public override int GetHashCode()
     {
-        return Array.Aggregate(0, (acc, item) => HashCode.Combine(acc, item));
+        return value.Aggregate(0, (acc, item) => HashCode.Combine(acc, item));
     }
 
     public override string ToString()
     {
-        return "[ " + string.Join(", ", Array) + " ]";
+        return "[ " + string.Join(", ", value) + " ]";
     }
 
     IEnumerator IEnumerable.GetEnumerator()
     {
-        return ((IEnumerable)Array).GetEnumerator();
+        return ((IEnumerable)value).GetEnumerator();
+    }
+
+    public T this[int index] => value[index];
+
+    public EqArray<T> Add(T item) => new(value.Add(item));
+
+    public EqArray<T> Replace(T oldItem, T newItem) => new(value.Replace(oldItem, newItem));
+}
+
+public static class EqArraySerdeWrap
+{
+    public readonly record struct SerializeImpl<T, TWrap>(EqArray<T> Value) : ISerialize
+        where TWrap : struct, ISerialize, ISerializeWrap<T, TWrap>
+    {
+        public void Serialize(ISerializer serializer)
+        {
+            EnumerableHelpers.SerializeSpan<T, TWrap>(typeof(EqArray<T>).Name, Value.Array.AsSpan(), serializer);
+        }
+    }
+    public readonly record struct DeserializeImpl<T, TWrap> : IDeserialize<EqArray<T>>
+        where TWrap : IDeserialize<T>
+    {
+        static EqArray<T> IDeserialize<EqArray<T>>.Deserialize<D>(ref D deserializer)
+        {
+            return deserializer.DeserializeEnumerable<
+                EqArray<T>,
+                Visitor>(new Visitor());
+        }
+
+        private struct Visitor : IDeserializeVisitor<EqArray<T>>
+        {
+            public string ExpectedTypeName => typeof(ImmutableArray<T>).ToString();
+            EqArray<T> IDeserializeVisitor<EqArray<T>>.VisitEnumerable<D>(ref D d)
+            {
+                ImmutableArray<T>.Builder builder;
+                if (d.SizeOpt is int size)
+                {
+                    builder = ImmutableArray.CreateBuilder<T>(size);
+                }
+                else
+                {
+                    size = -1; // Set initial size to unknown
+                    builder = ImmutableArray.CreateBuilder<T>();
+                }
+
+                while (d.TryGetNext<T, TWrap>(out T? next))
+                {
+                    builder.Add(next);
+                }
+                if (size >= 0 && builder.Count != size)
+                {
+                    throw new InvalidDeserializeValueException($"Expected {size} items, found {builder.Count}");
+                }
+                return new(builder.ToImmutable());
+            }
+        }
     }
 }
