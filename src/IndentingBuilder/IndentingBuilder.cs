66b0999c428061fc16fe0744ec278950a01c6884
Andy Gocke <andy@commentout.net>
2025-11-21 20:14:42 -0800

    Add IndentingBuilder for formatting source code

---
+++ src/IndentingBuilder/IndentingBuilder.cs
@@ -0,0 +1,243 @@
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace StaticCs;
+
+/// <summary>
+/// A string builder with automatic indentation support for multi-line text.
+/// Manages indentation levels with <see cref="Indent"/> and <see cref="Dedent"/> methods,
+/// and automatically applies the current indentation to appended content and interpolated strings.
+/// </summary>
+public sealed class IndentingBuilder : IComparable<IndentingBuilder>, IEquatable<IndentingBuilder>
+{
+    public static readonly Encoding UTF8Encoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false);
+
+    private string _currentIndentWhitespace = "";
+    private StringBuilder _stringBuilder;
+
+    public IndentingBuilder(string s)
+    {
+        _stringBuilder = new StringBuilder(s);
+    }
+
+    public IndentingBuilder(SourceBuilderStringHandler s)
+    {
+        _currentIndentWhitespace = "";
+        _stringBuilder = s._stringBuilder;
+    }
+
+    public IndentingBuilder()
+    {
+        _stringBuilder = new StringBuilder();
+    }
+
+    /// <summary>
+    /// Removes trailing whitespace from every line and replace all newlines with
+    /// Environment.NewLine.
+    /// </summary>
+    private void Normalize()
+    {
+        _stringBuilder.Replace("\r\n", "\n");
+
+        // Remove trailing whitespace from every line
+        int wsStart;
+        for (int i = 0; i < _stringBuilder.Length; i++)
+        {
+            if (_stringBuilder[i] is '\n')
+            {
+                wsStart = i - 1;
+                while (wsStart >= 0 && (_stringBuilder[wsStart] is ' ' or '\t'))
+                {
+                    wsStart--;
+                }
+                wsStart++; // Move back to first whitespace
+                if (wsStart < i)
+                {
+                    int len = i - wsStart;
+                    _stringBuilder.Remove(wsStart, len);
+                    i -= len;
+                }
+            }
+        }
+
+        _stringBuilder.Replace("\n", Environment.NewLine);
+    }
+
+    public override string ToString()
+    {
+        Normalize();
+        return _stringBuilder.ToString();
+    }
+
+    public void Append(
+        [InterpolatedStringHandlerArgument("")]
+        SourceBuilderStringHandler s)
+    {
+        // No work needed, the handler has already added the text to the string builder
+    }
+
+    public void Append(string s)
+    {
+        _stringBuilder.Append(_currentIndentWhitespace);
+        Append(_stringBuilder, _currentIndentWhitespace, s);
+    }
+
+    public void Append(IndentingBuilder srcBuilder)
+    {
+        Append(srcBuilder.ToString());
+    }
+
+    private static void Append(
+        StringBuilder builder,
+        string currentIndentWhitespace,
+        string str)
+    {
+        int start = 0;
+        int nl;
+        while (start < str.Length)
+        {
+            nl = str.IndexOf('\n', start);
+            if (nl == -1)
+            {
+                nl = str.Length;
+            }
+            // Skip blank lines
+            while (nl < str.Length && (str[nl] == '\n' || str[nl] == '\r'))
+            {
+                nl++;
+            }
+            if (start > 0)
+            {
+                builder.Append(currentIndentWhitespace);
+            }
+            builder.Append(str, start, nl - start);
+            start = nl;
+        }
+    }
+
+    public void AppendLine(
+        [InterpolatedStringHandlerArgument("")]
+        SourceBuilderStringHandler s)
+    {
+        Append(s);
+        _stringBuilder.AppendLine();
+    }
+
+    public void AppendLine(string s)
+    {
+        Append(s);
+        _stringBuilder.AppendLine();
+    }
+
+    public int CompareTo(IndentingBuilder? other)
+    {
+        if (other is null) return 1;
+
+        var lenCmp = _stringBuilder.Length.CompareTo(other._stringBuilder.Length);
+        if (lenCmp != 0)
+        {
+            return lenCmp;
+        }
+        for (int i = 0; i < _stringBuilder.Length; i++)
+        {
+            var cCmp = _stringBuilder[i].CompareTo(other._stringBuilder[i]);
+            if (cCmp != 0)
+            {
+                return cCmp;
+            }
+        }
+        return 0;
+    }
+
+    public void Indent()
+    {
+        _currentIndentWhitespace += "    ";
+    }
+
+    public void Dedent()
+    {
+        _currentIndentWhitespace = _currentIndentWhitespace[..^4];
+    }
+
+    public bool Equals(IndentingBuilder? other)
+    {
+        return _stringBuilder.Equals(other?._stringBuilder);
+    }
+
+    public void AppendLine(IndentingBuilder deserialize)
+    {
+        Append(deserialize);
+        _stringBuilder.AppendLine();
+    }
+
+    [InterpolatedStringHandler]
+    public ref struct SourceBuilderStringHandler
+    {
+        internal readonly StringBuilder _stringBuilder;
+        private readonly string _originalIndentWhitespace;
+        private string _currentIndentWhitespace;
+        private bool _isFirst = true;
+
+        public SourceBuilderStringHandler(int literalLength, int formattedCount)
+        {
+            _stringBuilder = new StringBuilder(literalLength);
+            _originalIndentWhitespace = "";
+            _currentIndentWhitespace = "";
+        }
+
+        public SourceBuilderStringHandler(
+            int literalLength,
+            int formattedCount,
+            IndentingBuilder sourceBuilder)
+        {
+            _stringBuilder = sourceBuilder._stringBuilder;
+            _originalIndentWhitespace = sourceBuilder._currentIndentWhitespace;
+            _currentIndentWhitespace = sourceBuilder._currentIndentWhitespace;
+        }
+
+        public void AppendLiteral(string s)
+        {
+            if (_isFirst)
+            {
+                _stringBuilder.Append(_currentIndentWhitespace);
+                _isFirst = false;
+            }
+            Append(_stringBuilder, _currentIndentWhitespace, s);
+
+            int last = s.LastIndexOf('\n');
+            if (last == -1)
+            {
+                return;
+            }
+
+            var remaining = s.AsSpan(last + 1);
+            foreach (var c in remaining)
+            {
+                if (c is not (' ' or '\t'))
+                {
+                    return;
+                }
+            }
+
+            _currentIndentWhitespace += remaining.ToString();
+        }
+
+        public void AppendFormatted<T>(T value)
+        {
+            if (_isFirst)
+            {
+                _stringBuilder.Append(_currentIndentWhitespace);
+                _isFirst = false;
+            }
+            var str = value?.ToString();
+            if (str is null)
+            {
+                _stringBuilder.Append(str);
+                return;
+            }
+
+            Append(_stringBuilder, _currentIndentWhitespace, str);
+            _currentIndentWhitespace = _originalIndentWhitespace;
+        }
+    }
+}
---
+++ test/test/IndentingBuilderTests.cs
@@ -0,0 +1,412 @@
+using System;
+using System.Text;
+using Xunit;
+
+namespace StaticCs.Tests;
+
+public sealed class IndentingBuilderTests
+{
+    [Fact]
+    public void Constructor_Default_CreatesEmptyBuilder()
+    {
+        var builder = new IndentingBuilder();
+        Assert.Equal("", builder.ToString());
+    }
+
+    [Fact]
+    public void Constructor_WithString_InitializesWithContent()
+    {
+        var builder = new IndentingBuilder("Hello, World!");
+        Assert.Equal("Hello, World!", builder.ToString());
+    }
+
+    [Fact]
+    public void Constructor_WithInterpolatedString_InitializesWithContent()
+    {
+        var name = "World";
+        var builder = new IndentingBuilder($"Hello, {name}!");
+        Assert.Equal("Hello, World!", builder.ToString());
+    }
+
+    [Fact]
+    public void Append_SimpleString_AppendsContent()
+    {
+        var builder = new IndentingBuilder();
+        builder.Append("Hello");
+        Assert.Equal("Hello", builder.ToString());
+    }
+
+    [Fact]
+    public void AppendLine_SimpleString_AppendsWithNewline()
+    {
+        var builder = new IndentingBuilder();
+        builder.AppendLine("Hello");
+        var expected = "Hello" + Environment.NewLine;
+        Assert.Equal(expected, builder.ToString());
+    }
+
+    [Fact]
+    public void AppendLine_MultipleLines_PreservesNewlines()
+    {
+        var builder = new IndentingBuilder();
+        builder.AppendLine("Line 1");
+        builder.AppendLine("Line 2");
+        var expected = "Line 1" + Environment.NewLine + "Line 2" + Environment.NewLine;
+        Assert.Equal(expected, builder.ToString());
+    }
+
+    [Fact]
+    public void Append_IndentingBuilder_AppendsContent()
+    {
+        var builder1 = new IndentingBuilder();
+        builder1.Append("Hello");
+
+        var builder2 = new IndentingBuilder();
+        builder2.Append(builder1);
+
+        Assert.Equal("Hello", builder2.ToString());
+    }
+
+    [Fact]
+    public void AppendLine_WithIndentingBuilder_AppendsWithNewline()
+    {
+        var builder1 = new IndentingBuilder();
+        builder1.Append("Inner content");
+
+        var builder2 = new IndentingBuilder();
+        builder2.AppendLine(builder1);
+        builder2.Append("Next line");
+
+        var expected = "Inner content" + Environment.NewLine + "Next line";
+        Assert.Equal(expected, builder2.ToString());
+    }
+
+    [Fact]
+    public void Indent_IncreasesIndentation()
+    {
+        var builder = new IndentingBuilder();
+        builder.AppendLine("Level 0");
+        builder.Indent();
+        builder.AppendLine("Level 1");
+        builder.Indent();
+        builder.AppendLine("Level 2");
+
+        var expected = "Level 0" + Environment.NewLine +
+                      "    Level 1" + Environment.NewLine +
+                      "        Level 2" + Environment.NewLine;
+        Assert.Equal(expected, builder.ToString());
+    }
+
+    [Fact]
+    public void Dedent_DecreasesIndentation()
+    {
+        var builder = new IndentingBuilder();
+        builder.AppendLine("Level 0");
+        builder.Indent();
+        builder.AppendLine("Level 1");
+        builder.Dedent();
+        builder.AppendLine("Back to Level 0");
+
+        var expected = "Level 0" + Environment.NewLine +
+                      "    Level 1" + Environment.NewLine +
+                      "Back to Level 0" + Environment.NewLine;
+        Assert.Equal(expected, builder.ToString());
+    }
+
+    [Fact]
+    public void Indent_Dedent_MultipleLevel_WorksCorrectly()
+    {
+        var builder = new IndentingBuilder();
+        builder.AppendLine("Level 0");
+        builder.Indent();
+        builder.AppendLine("Level 1");
+        builder.Indent();
+        builder.AppendLine("Level 2");
+        builder.Indent();
+        builder.AppendLine("Level 3");
+        builder.Dedent();
+        builder.Dedent();
+        builder.AppendLine("Level 1");
+        builder.Dedent();
+        builder.AppendLine("Level 0");
+
+        var expected = "Level 0" + Environment.NewLine +
+                      "    Level 1" + Environment.NewLine +
+                      "        Level 2" + Environment.NewLine +
+                      "            Level 3" + Environment.NewLine +
+                      "    Level 1" + Environment.NewLine +
+                      "Level 0" + Environment.NewLine;
+        Assert.Equal(expected, builder.ToString());
+    }
+
+    [Fact]
+    public void Append_StringWithNewlines_PreservesIndentation()
+    {
+        var builder = new IndentingBuilder();
+        builder.Indent();
+        builder.Append("Line 1\nLine 2\nLine 3");
+
+        var expected = "    Line 1" + Environment.NewLine +
+                      "    Line 2" + Environment.NewLine +
+                      "    Line 3";
+        Assert.Equal(expected, builder.ToString());
+    }
+
+    [Fact]
+    public void Normalize_RemovesTrailingWhitespace()
+    {
+        var builder = new IndentingBuilder();
+        builder.AppendLine("Hello   ");
+        builder.AppendLine("World\t\t");
+
+        var expected = "Hello" + Environment.NewLine + "World" + Environment.NewLine;
+        Assert.Equal(expected, builder.ToString());
+    }
+
+    [Fact]
+    public void Normalize_ConvertsAllNewlinesToEnvironmentNewline()
+    {
+        var builder = new IndentingBuilder("Line1\nLine2\r\nLine3");
+        var result = builder.ToString();
+
+        Assert.Contains("Line1" + Environment.NewLine, result);
+        Assert.Contains("Line2" + Environment.NewLine, result);
+        Assert.Contains("Line3", result);
+    }
+
+    [Fact]
+    public void CompareTo_EqualBuilders_ReturnsZero()
+    {
+        var builder1 = new IndentingBuilder("Test");
+        var builder2 = new IndentingBuilder("Test");
+
+        Assert.Equal(0, builder1.CompareTo(builder2));
+    }
+
+    [Fact]
+    public void CompareTo_DifferentLengths_ReturnsNonZero()
+    {
+        var builder1 = new IndentingBuilder("Short");
+        var builder2 = new IndentingBuilder("Longer String");
+
+        Assert.True(builder1.CompareTo(builder2) < 0);
+        Assert.True(builder2.CompareTo(builder1) > 0);
+    }
+
+    [Fact]
+    public void CompareTo_DifferentContent_ReturnsNonZero()
+    {
+        var builder1 = new IndentingBuilder("Apple");
+        var builder2 = new IndentingBuilder("Banana");
+
+        Assert.True(builder1.CompareTo(builder2) < 0);
+        Assert.True(builder2.CompareTo(builder1) > 0);
+    }
+
+    [Fact]
+    public void CompareTo_WithNull_ReturnsPositive()
+    {
+        var builder = new IndentingBuilder("Test");
+        Assert.True(builder.CompareTo(null) > 0);
+    }
+
+    [Fact]
+    public void Equals_SameContent_ReturnsTrue()
+    {
+        var builder1 = new IndentingBuilder("Test");
+        var builder2 = new IndentingBuilder("Test");
+
+        Assert.True(builder1.Equals(builder2));
+    }
+
+    [Fact]
+    public void Equals_DifferentContent_ReturnsFalse()
+    {
+        var builder1 = new IndentingBuilder("Test1");
+        var builder2 = new IndentingBuilder("Test2");
+
+        Assert.False(builder1.Equals(builder2));
+    }
+
+    [Fact]
+    public void Equals_WithNull_ReturnsFalse()
+    {
+        var builder = new IndentingBuilder("Test");
+        Assert.False(builder.Equals(null));
+    }
+
+    [Fact]
+    public void AppendLine_WithIndentingBuilder_WorksCorrectly()
+    {
+        var builder1 = new IndentingBuilder();
+        builder1.Append("Inner content");
+
+        var builder2 = new IndentingBuilder();
+        builder2.AppendLine(builder1);
+
+        var expected = "Inner content" + Environment.NewLine;
+        Assert.Equal(expected, builder2.ToString());
+    }
+
+    [Fact]
+    public void AppendLine_WithIndentingBuilder_PreservesIndentation()
+    {
+        var builder1 = new IndentingBuilder();
+        builder1.Append("First");
+        builder1.AppendLine("Second");
+
+        var builder2 = new IndentingBuilder();
+        builder2.Indent();
+        builder2.AppendLine(builder1);
+        builder2.Append("After");
+
+        var expected = "    FirstSecond" + Environment.NewLine + 
+                      Environment.NewLine + 
+                      "    After";
+        Assert.Equal(expected, builder2.ToString());
+    }
+
+    [Fact]
+    public void InterpolatedString_SimpleValues_WorksCorrectly()
+    {
+        var builder = new IndentingBuilder();
+        var name = "World";
+        var number = 42;
+        builder.Append($"Hello {name}, the answer is {number}");
+
+        Assert.Equal("Hello World, the answer is 42", builder.ToString());
+    }
+
+    [Fact]
+    public void InterpolatedString_StartingWithValue_AppliesIndentation()
+    {
+        var builder = new IndentingBuilder();
+        builder.Indent();
+        var value = "test";
+        builder.Append($"{value} is the value");
+
+        Assert.Equal("    test is the value", builder.ToString());
+    }
+
+    [Fact]
+    public void InterpolatedString_WithIndentation_WorksCorrectly()
+    {
+        var builder = new IndentingBuilder();
+        builder.Indent();
+        var value = "test";
+        builder.AppendLine($"Value: {value}");
+
+        var expected = "    Value: test" + Environment.NewLine;
+        Assert.Equal(expected, builder.ToString());
+    }
+
+    [Fact]
+    public void InterpolatedString_WithMultilineContent_PreservesIndentation()
+    {
+        var builder = new IndentingBuilder();
+        builder.Indent();
+        var multiline = "Line1\nLine2";
+        builder.Append($"Start\n{multiline}\nEnd");
+
+        // After a formatted value, indentation resets to the original level
+        var expected = "    Start" + Environment.NewLine +
+                      "Line1" + Environment.NewLine +
+                      "    Line2" + Environment.NewLine +
+                      "    End";
+        Assert.Equal(expected, builder.ToString());
+    }
+
+    [Fact]
+    public void EmptyBuilder_ToString_ReturnsEmptyString()
+    {
+        var builder = new IndentingBuilder();
+        Assert.Equal("", builder.ToString());
+    }
+
+    [Fact]
+    public void ComplexScenario_MixedIndentationAndContent_WorksCorrectly()
+    {
+        var builder = new IndentingBuilder();
+        builder.AppendLine("public class Example");
+        builder.AppendLine("{");
+        builder.Indent();
+        builder.AppendLine("public void Method()");
+        builder.AppendLine("{");
+        builder.Indent();
+        builder.AppendLine("Console.WriteLine(\"Hello\");");
+        builder.AppendLine("Console.WriteLine(\"World\");");
+        builder.Dedent();
+        builder.AppendLine("}");
+        builder.Dedent();
+        builder.AppendLine("}");
+
+        var expected = "public class Example" + Environment.NewLine +
+                      "{" + Environment.NewLine +
+                      "    public void Method()" + Environment.NewLine +
+                      "    {" + Environment.NewLine +
+                      "        Console.WriteLine(\"Hello\");" + Environment.NewLine +
+                      "        Console.WriteLine(\"World\");" + Environment.NewLine +
+                      "    }" + Environment.NewLine +
+                      "}" + Environment.NewLine;
+        Assert.Equal(expected, builder.ToString());
+    }
+
+    [Fact]
+    public void Append_BlankLines_PreservesBlankLines()
+    {
+        var builder = new IndentingBuilder();
+        builder.Indent();
+        builder.Append("Line 1\n\nLine 3");
+
+        var result = builder.ToString();
+        Assert.Contains("Line 1" + Environment.NewLine, result);
+        Assert.Contains("Line 3", result);
+    }
+
+    [Fact]
+    public void ToString_CalledMultipleTimes_ReturnsConsistentResults()
+    {
+        var builder = new IndentingBuilder();
+        builder.AppendLine("Test  ");
+
+        var result1 = builder.ToString();
+        var result2 = builder.ToString();
+
+        Assert.Equal(result1, result2);
+        Assert.Equal("Test" + Environment.NewLine, result1);
+    }
+
+    [Fact]
+    public void InterpolatedString_WithNullValue_HandlesGracefully()
+    {
+        var builder = new IndentingBuilder();
+        string? nullValue = null;
+        builder.Append($"Value: {nullValue}");
+
+        Assert.Equal("Value: ", builder.ToString());
+    }
+
+    [Fact]
+    public void MultipleIndents_ThenDedents_MaintainsCorrectLevel()
+    {
+        var builder = new IndentingBuilder();
+        builder.AppendLine("L0");
+
+        for (int i = 1; i <= 5; i++)
+        {
+            builder.Indent();
+            builder.AppendLine($"L{i}");
+        }
+
+        for (int i = 4; i >= 0; i--)
+        {
+            builder.Dedent();
+            builder.AppendLine($"Back to L{i}");
+        }
+
+        var result = builder.ToString();
+        Assert.StartsWith("L0" + Environment.NewLine, result);
+        Assert.Contains("                    L5" + Environment.NewLine, result); // 20 spaces (5 * 4)
+        Assert.EndsWith("Back to L0" + Environment.NewLine, result);
+    }
+}
